6b12ed066c3c078147506b413b7ca6331874fcf1
FergalOK <fergal.okane@yahoo.com>
2021-08-27 16:30:02 +0200

    Create `externalController` for bus timings

--- cpu6502.cpp
+++ cpu6502.cpp
@@ -17,3 +17,39 @@ class cpu6502Interface
     // Bi-directional
     uint8_t dataBus = 0; // pins 21-28 (D0-D7)
 };
+
+class externalController
+{
+    bool phaseOne = false;
+    bool phaseTwo = false;
+
+    void cycle()
+    {
+        // Wait rise time (max 25ns)
+        phaseOne = true;
+        // Wait max(R/W setup, addr setup) (typically 200ns)
+        bool reading = true; // R/W from CPU
+        uint16_t addr = 0; // address from CPU
+
+        if (reading)
+        {
+            // Wait for Memory to read address and set data (max 500ns)
+            // Pulse changes midway through
+            phaseOne = false;
+            phaseTwo = true;
+            uint8_t data = 0;
+            // Wait for CPU to read data (min 100ns)
+            phaseTwo = false;
+            // Data should be kept for min 10ns after the end of the cycle
+        }
+        else
+        {
+            // Wait for phase 2 + data setup time
+            phaseOne = false;
+            phaseTwo = true;
+            // Wait for write to Memory
+            phaseTwo = false;
+            // Data should be kept for min 10ns after the end of the cycle
+        }
+    }
+};
