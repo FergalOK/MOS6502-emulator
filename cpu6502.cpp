48dca8c5db05868ca68e8cbc5ee932ac0b39f76c
FergalOK <fergal.okane@yahoo.com>
2021-08-27 16:47:54 +0200

    Add R/W between Memory and CPU

--- cpu6502.cpp
+++ cpu6502.cpp
@@ -3,6 +3,7 @@
 
 class cpu6502Interface
 {
+    public:
     // Input
     void tick(); // pin 29 (CLK)
     void reset(); // pin 03 (¬RST)
@@ -20,33 +21,40 @@ class cpu6502Interface
 
 class externalController
 {
+    cpu6502Interface interface;
     bool phaseOne = false;
     bool phaseTwo = false;
+    uint8_t memory[2048];
+    uint8_t data;
 
     void cycle()
     {
         // Wait rise time (max 25ns)
         phaseOne = true;
         // Wait max(R/W setup, addr setup) (typically 200ns)
-        bool reading = true; // R/W from CPU
-        uint16_t addr = 0; // address from CPU
+        bool reading = interface.reading; // R/W from CPU
+        uint16_t addr = interface.addressBus; // address from CPU
 
         if (reading)
         {
+            data = memory[addr];
             // Wait for Memory to read address and set data (max 500ns)
             // Pulse changes midway through
             phaseOne = false;
             phaseTwo = true;
-            uint8_t data = 0;
+            interface.dataBus = data;
             // Wait for CPU to read data (min 100ns)
             phaseTwo = false;
             // Data should be kept for min 10ns after the end of the cycle
         }
         else
         {
-            // Wait for phase 2 + data setup time
+            // Wait for phase 2
+            data = interface.dataBus; 
+            // data setup time
             phaseOne = false;
             phaseTwo = true;
+            memory[addr] = data;
             // Wait for write to Memory
             phaseTwo = false;
             // Data should be kept for min 10ns after the end of the cycle
