dca59dc6c92088028823cb010897030dd04d5ebd
FergalOK <fergal.okane@yahoo.com>
2021-08-28 14:24:54 +0200

    Create general framework for execution

--- cpu6502.cpp
+++ cpu6502.cpp
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <iostream>
+#include <stdio.h>
 
 class cpu6502Interface
 {
@@ -14,26 +15,80 @@ class cpu6502Interface
     uint16_t addressBus = 0; // pins 04-19 (A0-A15)
     bool reading = false; // pin 34 (R/W) Read when high, write when low
     uint8_t out = 0; // pins 37-39 (OUT2-OUT0) Only first 3 bits are used
+    bool sync = true;
 
     // Bi-directional
     uint8_t dataBus = 0; // pins 21-28 (D0-D7)
 };
 
+class internalController
+{
+    cpu6502Interface* interface;
+    // Hardware Manual:
+    // "Each instruction starts in T0 and goes to T1, T2, T3, etc. 
+    // for as many cycles as are required to complete execution of the instruction."
+    uint8_t timing = -2;
+    uint16_t pc = 0;
+    uint8_t instr = 0;
+    uint8_t lenData = 0;
+
+    uint8_t instrLen(uint8_t instr)
+    {
+        // TODO
+        return 3;
+    }
+
+    void execute(uint8_t instr)
+    {
+        // Execute instruction
+    }
+
+    public:
+    internalController(cpu6502Interface* _interface): interface(_interface) {}
+    void cycle()
+    {
+        timing++;
+        if (interface->sync)
+        {
+            interface->addressBus = pc;
+            interface->reading = true;
+            pc++;
+        }
+        if (timing == -1) return;
+
+        if (timing == 0)
+        {
+            instr = interface->dataBus;
+            uint8_t len = instrLen(instr);
+            execute(instr);
+            // If instruction only requires the instruction byte,
+            // the following fetched byte is discarded while the 1-byte
+            // instruction is executing 
+            if (len != 1) {
+                pc++;
+            }
+        }
+    }
+};
+
 class externalController
 {
-    cpu6502Interface interface;
+    cpu6502Interface* interface;
     bool phaseOne = false;
     bool phaseTwo = false;
     uint8_t memory[2048];
     uint8_t data;
+
+    public:
+    externalController(cpu6502Interface* _interface): interface(_interface) {}
 
     void cycle()
     {
         // Wait rise time (max 25ns)
         phaseOne = true;
         // Wait max(R/W setup, addr setup) (typically 200ns)
-        bool reading = interface.reading; // R/W from CPU
-        uint16_t addr = interface.addressBus; // address from CPU
+        bool reading = interface->reading; // R/W from CPU
+        uint16_t addr = interface->addressBus; // address from CPU
 
         if (reading)
         {
@@ -42,7 +97,7 @@ class externalController
             // Pulse changes midway through
             phaseOne = false;
             phaseTwo = true;
-            interface.dataBus = data;
+            interface->dataBus = data;
             // Wait for CPU to read data (min 100ns)
             phaseTwo = false;
             // Data should be kept for min 10ns after the end of the cycle
@@ -50,7 +105,7 @@ class externalController
         else
         {
             // Wait for phase 2
-            data = interface.dataBus; 
+            data = interface->dataBus; 
             // data setup time
             phaseOne = false;
             phaseTwo = true;
@@ -61,3 +116,25 @@ class externalController
         }
     }
 };
+
+class controller
+{
+    cpu6502Interface* interface = new cpu6502Interface();
+    internalController* internal = new internalController(interface);
+    externalController* external = new externalController(interface);
+
+    public:
+    void cycle()
+    {
+        // TODO: Use 2 threads?
+        internal->cycle();
+        external->cycle();
+    }
+};
+
+int main()
+{
+    controller controller;
+    controller.cycle();
+    controller.cycle();
+}
