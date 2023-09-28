9c22fb207de9ac7e9511b88ec290289e2b0d3948
fergal <fergal.okane@yahoo.com>
2023-09-28 20:54:09 +0200

    Add Clock method definitions

--- include/clock.hpp
+++ include/clock.hpp
@@ -1,9 +1,21 @@
 #ifndef CLOCK_HPP
 #define CLOCK_HPP
 
+#include <vector>
+
+class ClockSubscriber
+{
+public:
+    virtual void tick() = 0;
+};
+
 class Clock
 {
+private:
+    std::vector<ClockSubscriber> _subscribers;
 
+public:
+    void run();
 };
 
 #endif
