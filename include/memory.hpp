9961483f3806000fd920852bf109d0ad56a10b25
fergal <fergal.okane@yahoo.com>
2023-09-28 20:53:46 +0200

    Add Bus method definitions

--- include/memory.hpp
+++ include/memory.hpp
@@ -8,13 +8,21 @@
 // Programmer sets ROM, RAM, IO ranges
 uint8_t MEM[MEM_SIZE];
 
+class Bus
+{
+private:
+    uint8_t _data;
+public:
+    uint8_t read();
+    void write(uint8_t data);
+};
 
-class AddressBus
+class AddressBus: public Bus
 {
 
 };
 
-class DataBus
+class DataBus: public Bus
 {
 
 };
